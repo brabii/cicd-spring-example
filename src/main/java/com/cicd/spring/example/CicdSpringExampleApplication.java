101105c69296bd7c4f4220b5f9b75d1558fbf10a
brabii <collectorsgarbages@gmail.com>
2020-12-25 21:24:09 +0100

    créer projet spring-boot

---
+++ src/main/java/com/cicd/spring/example/CicdSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.spring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSpringExampleApplication.class, args);
+	}
+
+}
